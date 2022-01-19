aa86f1e1b90d0ce7f125c22ad449521d944c7328
Taruna Schuelke <tarunaschuelke@Tarunas-MacBook-Pro-2.local>
2022-01-18 21:46:26 -0800

    adding final R script

--- ExploringColorPalettes.R
+++ ExploringColorPalettes.R
@@ -1,29 +1,35 @@
+# install.packages("tidyverse")
 library(tidyverse)
 
 # create a random ASV table with relative abundance values in percentages
 asvTable <- matrix(c(70.93,78.58,78.72,69.24,25.53,43.85,7.49,
                      10.00,78.30,18.11,71.16,63.82,47.37,89.87),ncol=2)
+
+# reassign names to columns and rows
 colnames(asvTable) <- c("Before","After")
-rownames(asvTable) <- 1:7
 rownames(asvTable) <- c("ASV1", "ASV2", "ASV3", "ASV4", "ASV5", "ASV6", "ASV7")
+
+# check what the matrix looks like
 asvTable
 
-# melt the asvTable 
+# melt the asvTable so that the "y" types of values are in the same column
+# install.packages("reshape")
 library("reshape2")
 asvTable_melt <- melt(asvTable)
+
+# assign new names to columns. Renaming works it easier to call the cols when plotting.
 names(asvTable_melt) <- c("ASV", "Treatment", "RA_perc")
 asvTable_melt
 
 # create a bar plot with default colors
+# install.packages("ggplot")
 library(ggplot2)
 ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
-  geom_bar(stat="identity", position="dodge", colour="black") 
+  geom_bar(stat="identity", position="dodge") #, colour="black") 
   
 # create a bar plot with colors from the RColorBrewer package
 # install.packages("RColorBrewer")
 library(RColorBrewer)
-library(tmaptools)
-library(dichromat)
 
 # display all palettes in the RColorBrewer package
 display.brewer.all()
@@ -33,10 +39,12 @@ display.brewer.pal(n = 8, name = 'Dark2')
 brewer.pal(n = 8, name = "Dark2")
 
 # let us explore the color palettes in the RColorBrewer package in a Shiny app
+# install.packages("tmaptools")
+library(tmaptools)
 palette_explorer()
 
 # assign your chosen color palette to a variable instead of burying it in the ggplot code
-colors4ASVbars <- dichromat(get_brewer_pal("Dark2", n = 7))
+colors4ASVbars <- get_brewer_pal("Dark2", n = 7)
 
 # create the bar plot with the RColorBrewer color palette
 ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
@@ -45,12 +53,26 @@ ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
   xlab("Incubation Times") +
   scale_fill_manual(values = colors4ASVbars)
 
-########################## some fun color palettes ############################# 
-#https://rforpoliticalscience.com/2020/07/26/make-wes-anderson-themed-graphs-with-wesanderson-package-in-r/
-install.packages("wesanderson")
-library(wesanderson)
-myWAcolors <- wes_palette("BottleRocket1", n = 7)
+# lets see what the above colors appear as they do for folks with color blindness 
+# install.packages("dichromat")
+library(dichromat)
 
+# assign the RColorBrewer color palette to a variable and run the dichromat function as shown below
+colors4ASVbars_dichromat <- dichromat(colors4ASVbars, type = c("deutan", "protan", "tritan"))
+
+# plot with the new color palette
+ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
+  geom_bar(stat="identity", position="dodge", colour="black") +
+  ylab("Relative Abundance (%)") +  # add the title on y axis
+  xlab("Incubation Times") +
+  scale_fill_manual(values = colors4ASVbars_dichromat)
+
+########################## some fun color palettes ############################# 
+# install.packages("wesanderson")
+library(wesanderson)
+
+# assign a variable for the Wes Anderson Color palette 
+myWAcolors <- wes_palette("BottleRocket1", n = 7)
 
 # create the bar plot with the Wes Anderson color palette
 ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
@@ -59,7 +81,18 @@ ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
   xlab("Incubation Times") +
   scale_fill_manual(values = myWAcolors)
 
+# copy and paste the url below in a web browser
+# https://emilhvitfeldt.github.io/r-color-palettes/discrete.html
 # install.packages("paletteer")
 library(paletteer) 
-paletteer_d("beyonce::X4")
-paletteer_d("colorBlindness::paletteMartin")
+
+# assign a variable for your favorite color palette from Paletteer
+paletterColors <- paletteer_d("beyonce::X90")
+
+# create the bar plot with the Wes Anderson color palette
+ggplot(asvTable_melt, aes(x=Treatment, y=RA_perc, fill=factor(ASV))) +
+  geom_bar(stat="identity", position="dodge", colour="black") +
+  ylab("Relative Abundance (%)") +  # add the title on y axis
+  xlab("Incubation Times") +
+  scale_fill_manual(values = paletterColors)
+
